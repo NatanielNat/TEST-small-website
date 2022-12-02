0f8f53707d859f6fd6ca7f1e06341f14a2a5244d
Nataniel Kaczmarczyk <text-git@gmail.comgit configgit configgit config --global user.email text-git@gmail.com>
2022-12-02 20:52:17 +0100

    Added Normalize and functions

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
+{
+    const welcome= () => {
+        console.log("nie umiem jeszcze zmienic styli za pomaca js,a chcialem zmienic kolor hovera na nawigacje kiedy jest czarno biala :/ ");
+    }
+    welcome();
+}
+{
+    const toogleBackgroundColor = () =>{
 
-console.log("nie umiem jeszcze zmienic styli za pomaca js,a chcialem zmienic kolor hovera na nawigacje kiedy jest czarno biala :/ ")
-
-
-let przycisk = document.querySelector(".button")
-let background = document.querySelector(".container")
-
-przycisk.addEventListener("click", () => {
-    background.classList.toggle("hidden");
-
-    if (background.classList.contains("hidden")) { przycisk.innerText = "Zmien Kolor Na Czarno Bialy" }
-    else { przycisk.innerText = "Zmien Kolor Na Gradient" }
-
-});
+        let background = document.querySelector(".container");
+        
+         background.classList.toggle("hidden");
+        
+        if (background.classList.contains("hidden")) { przycisk.innerText = "Zmien Kolor Na Czarno Bialy" }
+        else { przycisk.innerText = "Zmien Kolor Na Gradient" }
+        
+        
+        };
+        let przycisk = document.querySelector(".button");
+        
+        przycisk.addEventListener("click",toogleBackgroundColor);
+        
+}
 
 
 
